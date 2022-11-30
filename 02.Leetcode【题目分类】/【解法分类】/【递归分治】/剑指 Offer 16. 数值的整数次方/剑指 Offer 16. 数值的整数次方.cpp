a8d8a0014ff2ff8a24677ebcf4021880b5d98d16
TommyLZ <lz1546297241@163.com>
2022-11-30 22:43:51 +0800

    2022/11/30

---
+++ 02.Leetcode【题目分类】/【解法分类】/【递归分治】/剑指 Offer 16. 数值的整数次方/剑指 Offer 16. 数值的整数次方.cpp
@@ -0,0 +1,29 @@
+// 递归 + 快速幂
+#include <iostream>
+using namespace std;
+class Solution{
+public:
+    double quickMul(double x, long long N) {
+        if (N==0){
+            return 1.0;
+        }
+        
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N); // 需要考虑到负数幂
+    }
+};
+
+int main() {
+    Solution solution;
+    double x = 2.10000;
+    long long n = 3;
+    double res = solution.myPow(x, n);
+
+    cout << "res: " << res << endl;
+    return 0;
+}
